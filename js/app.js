49e536c855d7c88cb8ed8fce31718cc57a6cf5ea
PedroLucasCG <Pedrowwww44@gmail.com>
2024-06-15 20:41:45 -0300

    adição da funcionalidade de inserção de funcionários

--- js/app.js
+++ js/app.js
@@ -15,3 +15,20 @@ function openWindow()
     let win = document.getElementsByClassName("pesquisar-colaboradores")[0]
     win.classList.toggle("fechar")
 }
+
+function toggleColaborador(item)
+{
+    item.classList.toggle("selecionado")
+    addColaborador(item)
+}
+
+function addColaborador(colaborador)
+{
+    const colaboradoresContainer = document.getElementsByClassName("colaboradores")[0].getElementsByTagName("div")[0];
+    const template = document.getElementsByTagName("template")[0];
+    
+    let item = template.content.cloneNode(true).children[0];
+    item.firstChild.nodeValue = colaborador.children[0].firstChild.data;
+
+    colaboradoresContainer.appendChild(item);
+}
