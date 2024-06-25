1e8205c9d97ac6f83dea4bd3bd9988b59d2c0515
PedroLucasCG <Pedrowwww44@gmail.com>
2024-06-25 18:20:58 -0300

    Adição de cometários no código JavaScript

--- js/app.js
+++ js/app.js
@@ -1,26 +1,29 @@
 
-//solicitar ordem de serviço
+//Acessa o menu de navegação de colaboradores através do tab e enter
 let add = document.getElementsByClassName("colaboradores")[0].getElementsByTagName("span")[0]
-add.addEventListener('keypress', function (e){
+add.addEventListener('keypress', function (e) {
     openWindow()
 })
 
+//Array que contém os colaboradores inseridos
 let colaboradoresInserted = [];
+
+//Remove os colaboradores retirados da lista de colaboradores inseridos
 const updateColaboradoresInserted = function (data) {
     colaboradoresInserted = colaboradoresInserted.filter(item => {
         return item != data
     })
 }
 
-function removeItem(item)
-{
+//Remove colaboradores selecionados
+function removeItem(item) {
     let data = item.previousSibling.data
     updateColaboradoresInserted(data)
 
     const lista = document.getElementsByClassName("lista")[0].getElementsByTagName("section")[0].children
     let aux;
     Array.from(lista).forEach(item => {
-        if(item.children[0].firstChild.data == data) aux = item
+        if (item.children[0].firstChild.data == data) aux = item
     })
 
     let span = item.parentNode
@@ -28,47 +31,48 @@ function removeItem(item)
     toggleColaborador(aux)
 }
 
-function closeWindow(window)
-{
+//Fecha a tela de seleção de colaboradores
+function closeWindow(window) {
     window = window.parentNode.parentNode
     window.classList.toggle("fechar")
 }
 
-function openWindow()
-{
+//Abre a tela de colaboradores
+function openWindow() {
     let win = document.getElementsByClassName("pesquisar-colaboradores")[0]
     win.classList.toggle("fechar")
 }
 
-function toggleColaborador(item)
-{
+//Adiciona ou retira o colaborador de maneira dinâmica
+function toggleColaborador(item) {
     let add = true
-    if(item.classList[0])
+    if (item.classList[0])
         add = false
     item.classList.toggle("selecionado")
-    if (add)
-    {
+    if (add) {
         addColaborador(item)
     }
-    else
-    {
-        const container = document.getElementsByClassName("colaboradores")[0].getElementsByTagName("div")[0].children
-        Array.from(container).forEach(child =>{
-            if(child.firstChild.data == item.children[0].firstChild.data)
-                child.remove()
-                updateColaboradoresInserted(child.firstChild.data)
-        })
+    else {
+        removeColaborador(item)
     }
 
 }
 
-function addColaborador(colaborador)
-{
+function removeColaborador(item) {
+    const container = document.getElementsByClassName("colaboradores")[0].getElementsByTagName("div")[0].children
+    Array.from(container).forEach(child => {
+        if (child.firstChild.data == item.children[0].firstChild.data)
+            child.remove()
+        updateColaboradoresInserted(child.firstChild.data)
+    })
+}
+
+function addColaborador(colaborador) {
     const colaboradoresContainer = document.getElementsByClassName("colaboradores")[0].getElementsByTagName("div")[0]
     const template = document.getElementsByTagName("template")[0]
     const data = colaborador.children[0].firstChild.data
 
-    if(colaboradoresInserted.includes(data)) return;
+    if (colaboradoresInserted.includes(data)) return;
 
     let item = template.content.cloneNode(true).children[0]
     item.firstChild.nodeValue = data
@@ -76,5 +80,3 @@ function addColaborador(colaborador)
 
     colaboradoresInserted.push(data)
 }
-
-//Encontrar ordem de serviço
--- js/app2.js
+++ js/app2.js
@@ -1,13 +1,18 @@
+//container dos indexes das páginas de O.S.
 const pageIndexContainer = document.getElementsByClassName("pages")[0]
+//Template dos indexes
 const indexTemp = document.getElementsByTagName("template")[1].content
 
+//Container das ordens de serviço
 const osContainer = document.getElementsByClassName("os-historico")[0];
+//Template das ordens de serviço
 const osTemp = document.getElementsByTagName("template")[0].content.childNodes[1]
 
 function loadEntries(pageIndex = 1) {
     //Quantidade de itens exibidos por páginas
     const qtdItens = 5
 
+    //Remove a as ordens de serviço exibidas na página anterior
     const items = osContainer.querySelectorAll("div[name='entry']")
     for (e in Array.from(items))
         items[e].remove()
@@ -15,17 +20,17 @@ function loadEntries(pageIndex = 1) {
     fetch('json/ordens.json')
         .then((response) => response.json())
         .then((data) => {
-            //constrói os índices de navegação das páginas
+            //Constrói os índices de navegação das páginas
             makeIndexes(data, qtdItens, pageIndex)
 
+            //Marca o index selecionado na exibição
             const currentIndexes = pageIndexContainer.querySelectorAll('span[type="index"]')
             for (e in Array.from(currentIndexes)){
                 if(currentIndexes[e].firstChild.data == pageIndex)
                     currentIndexes[e].classList.add("current")
             }
-            //console.log(currentIndex)
 
-            //constrói os elementos HTML das O.S.
+            //Constrói os elementos HTML das O.S.
             for (entry in data) {
                 const ini = qtdItens * (pageIndex - 1)
                 if (parseInt(entry) == ini + qtdItens) {
@@ -57,12 +62,23 @@ function makeIndexes(data, qtdItens, pageIndex){
     const numberOfIndexes = Math.floor(total/qtdItens) == total/qtdItens ? total/qtdItens : Math.floor(total/qtdItens)+1
     const template = indexTemp.cloneNode()
     
+    //Define quantos itens são exibidos em tela dependendo do número de indices e o range (número de indexes exibidos além do atual)
     let end = numberOfIndexes
     let start = pageIndex
     let range = 4
+
+    //Caso o número de index for menor ou igual ao range é calculado um range fixo indenpendente do especificado acima
     if (numberOfIndexes <= range)
         range = numberOfIndexes-1
     
+    /*
+    *Sequência de condicionais que lidam com diferetes casos
+    *Primeira: checa se o index atual é o último, se for o range se expande totalmente a esquerda do index atual
+    *Segunda: verifica se o index atual é o primeiro, se for o range se expande totalmente a direita do index atual
+    *Terceira e quarta: verifica se o index atual se localiza próximo a uma extremidade impossibilitando ele se localizar no meio, o início, ou o fim é mantido no primeiro item
+    e o fim, ou início, é o index atual mais/menos a metade do range +/- 1, mantendo o range especificado de intens na tela
+    *O else lida é quando o index atual está localizado a uma distância que possibilita ele ficar no meio, range/2 para esqueda e para a direita
+    */
     if (pageIndex == numberOfIndexes){
         start = pageIndex - range
 
